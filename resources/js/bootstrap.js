40e0045a23ac5bab52468235654fe222b0cba666
Ahmed Zaky <a.zaki@benefits-hero.com>
2022-12-31 23:29:22 +0200

    add laravel websockets package and setup the right configurations

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -50,5 +50,9 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: true
+    forceTLS: false,
+    encrypted: false,
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    enabledTransports: ['ws', 'wss'],
 });
