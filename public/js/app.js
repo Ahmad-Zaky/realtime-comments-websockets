deebafce967a804ed2c61564f212995d30160334
Ahmed Zaky <a.zaki@benefits-hero.com>
2023-01-02 16:46:34 +0200

    compiled assets

--- public/js/app.js
+++ public/js/app.js
@@ -24658,11 +24658,18 @@ if (token) {
 
 
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
+window.appKey = "ABCDEFG";
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "48b34187c6598b309fff",
-  cluster: "sa1",
-  forceTLS: true
+  key: "ABCDEFG",
+  cluster: "mt1",
+  forceTLS: "http" === 'https',
+  encrypted: "http" === 'https',
+  disableStats: "http" === 'https',
+  wsHost: window.location.hostname,
+  wsPort: "6001",
+  wssPort: "6001",
+  enabledTransports: ['ws', 'wss']
 });
 
 /***/ }),
@@ -57132,6 +57139,32 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/css/helper.css":
+/*!**********************************!*\
+  !*** ./resources/css/helper.css ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./resources/css/chat.css":
+/*!********************************!*\
+  !*** ./resources/css/chat.css ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./resources/css/app.css":
 /*!*******************************!*\
   !*** ./resources/css/app.css ***!
@@ -63365,7 +63398,9 @@ module.exports = JSON.parse('{"_from":"axios@^0.21","_id":"axios@0.21.4","_inBun
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = {
 /******/ 			"/js/app": 0,
-/******/ 			"css/app": 0
+/******/ 			"css/app": 0,
+/******/ 			"css/chat": 0,
+/******/ 			"css/helper": 0
 /******/ 		};
 /******/ 		
 /******/ 		// no chunk on demand loading
@@ -63415,9 +63450,11 @@ module.exports = JSON.parse('{"_from":"axios@^0.21","_id":"axios@0.21.4","_inBun
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/sass/app.scss")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/css/app.css")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/chat","css/helper"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/chat","css/helper"], () => (__webpack_require__("./resources/sass/app.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/chat","css/helper"], () => (__webpack_require__("./resources/css/helper.css")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/chat","css/helper"], () => (__webpack_require__("./resources/css/chat.css")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app","css/chat","css/helper"], () => (__webpack_require__("./resources/css/app.css")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
